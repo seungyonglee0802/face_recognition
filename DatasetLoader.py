fc961725a7bee7e53716c0b88baba67144d41eef
seungyongLee <sylee080299@gmail.com>
2022-11-13 14:31:25 +0900

    upload exps scores

--- DatasetLoader.py
+++ DatasetLoader.py
@@ -21,18 +21,20 @@ class meta_loader(Dataset):
     def __init__(self, train_path, train_ext, transform):
         
         ## Read Training Files
+        # data/train/id10051/B00000000.jpg
         files = glob.glob('%s/*/*.%s'%(train_path,train_ext))
 
         ## Make a mapping from Class Name to Class Number
         dictkeys = list(set([x.split('/')[-2] for x in files]))
         dictkeys.sort()
+        # {id10051: 0, id10052: 1, ...}
         dictkeys = { key : ii for ii, key in enumerate(dictkeys) }
 
         self.transform  = transform
 
-        self.label_dict = {}
-        self.data_list  = []
-        self.data_label = []
+        self.label_dict = {} #{0: [0,1,...], 1: [126,127,...], 2: [...]}
+        self.data_list  = [] #[data/train/id10051/B00000000.jpg, data/train/id10051/B00000001.jpg, ...]
+        self.data_label = [] #[0, 0, ..., 1, ..., 2, ...]
         
         for lidx, file in enumerate(files):
             speaker_name = file.split('/')[-2]
@@ -49,11 +51,11 @@ class meta_loader(Dataset):
         print('{:d} files from {:d} classes found.'.format(len(self.data_list),len(self.label_dict)))
 
     def __getitem__(self, indices):
-
+        # indices: batch indices
         feat = []
         for index in indices:
             feat.append(self.transform(Image.open(self.data_list[index])));
-        feat = numpy.stack(feat, axis=0)
+        feat = numpy.stack(feat, axis=0) #[len_indices, C, W, H]
 
         return torch.FloatTensor(feat), self.data_label[index]
 
@@ -67,6 +69,7 @@ class test_dataset_loader(Dataset):
         self.data_list  = test_list
         self.transform  = transform
 
+    ## test one sample from the data_list
     def __getitem__(self, index):
         img = Image.open(os.path.join(self.test_path, self.data_list[index]))
         return self.transform(img), self.data_list[index]
@@ -88,23 +91,23 @@ class meta_sampler(torch.utils.data.Sampler):
     def __iter__(self):
         
         ## Get a list of identities
-        dictkeys = list(self.label_dict.keys());
+        dictkeys = list(self.label_dict.keys()); # [id10051, id10052, ...]
         dictkeys.sort()
 
-        lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+        lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)] # chop lst w.r.t sz(size) # [len(lst)/sz, sz]
 
         flattened_list = []
         flattened_label = []
 
         ## Data for each class
         for findex, key in enumerate(dictkeys):
-            data    = self.label_dict[key]
+            data    = self.label_dict[key] # sample idices for id10051
             numSeg  = round_down(min(len(data),self.max_img_per_cls),self.nPerClass)
             
             rp      = lol(numpy.random.permutation(len(data))[:numSeg],self.nPerClass)
             flattened_label.extend([findex] * (len(rp)))
             for indices in rp:
-                flattened_list.append([data[i] for i in indices])
+                flattened_list.append([data[i] for i in indices]) #[[0,2,5,6,9], [1,3,4,7,8]] rp == 2, nPerClass == 5
 
         ## Data in random order
         mixid           = numpy.random.permutation(len(flattened_label))
@@ -113,12 +116,12 @@ class meta_sampler(torch.utils.data.Sampler):
 
         ## Prevent two pairs of the same speaker in the same batch
         for ii in mixid:
-            startbatch = len(mixlabel) - len(mixlabel) % self.batch_size
-            if flattened_label[ii] not in mixlabel[startbatch:]:
+            startbatch = len(mixlabel) - len(mixlabel) % self.batch_size # index where this batch start
+            if flattened_label[ii] not in mixlabel[startbatch:]: # randomly choose diff identities(batch size)
                 mixlabel.append(flattened_label[ii])
                 mixmap.append(ii)
 
-        batch_indices = [flattened_list[i] for i in mixmap]
+        batch_indices = [flattened_list[i] for i in mixmap] # for chosen identities get nPerClass samples per each identity #[batch_size, nPerClass]
 
         self.num_iters = len(batch_indices)
 
--- trainEmbedNet.py
+++ trainEmbedNet.py
@@ -82,7 +82,11 @@ def main_worker(args):
     train_transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Resize(256),
-         transforms.RandomCrop([224,224]),
+         transforms.RandomResizedCrop([224,224], scale=(0.8, 1)),
+         transforms.RandomHorizontalFlip(),
+         transforms.ColorJitter(brightness=0.5),
+         transforms.ColorJitter(contrast=0.5),
+         transforms.ColorJitter(saturation=0.5),
          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
     ## Input transformations for evaluation
